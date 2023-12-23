6b8c8cbf0bdcc1479b4796c14f205edb5a0fa644
Saya <saianakimovna2gmail.com>
2023-12-23 17:04:18 +0000

    new files HWpart2

---
+++ Homework2/HWpart2.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "part2" {
+    ami    = data.aws_ami.amzn-linux-2023-ami.id
+    instance_type = "t2.micro"
+    key_name = aws_key_pair.deployer
+
+}
+resource "aws_security_group" "part2sg" {
+    name  = "my-test"
+    description = "for HWpart2"
+    ingress {
+       from_port        = 0
+       to_port          = 0
+       protocol         = "-1"
+       cidr_blocks      = ["0.0.0.0/0"]
+    }
+     egress 
+       from_port        = 0
+       to_port          = 0
+       protocol         = "-1"
+       cidr_blocks      = ["0.0.0.0/0"]
+     }
+}
